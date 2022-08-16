62e70fb03eb52b11ccd9a365dbfeb699e5af323e
nour-soussi <soussinour2001@gmail.com>
2022-08-16 14:55:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state =  {
+        fullName : "flen",
+        bio:"brabbi 10 ye mr ",
+        img:'image.png' ,
+        profession :"developpeur ",
+        show:false , 
+        count:0,
+        interId:0
+
+    
 }
+componentDidMount() {
+const interId=setInterval(()=>{
+  this.setState({count:this.state.count+1})
+},
+    1000
+  );
+  console.log(this.state.count)
+}
+
+/* componentWillUnmount() {
+  clearInterval(this.state.interId);
+} */
+
+handleShow = () => {
+  if( this.state.show){
+    this.setState({ show: false });
+    }
+    
+    else{ this.setState({ show: true });
+}
+};
+
+
+  render() {
+    return (
+    <div>
+          <h2>the time interval since the last component was mounted {this.state.count}</h2>
+          <h2>{this.state.bio}</h2>
+          <button onClick={this.handleShow}>change</button>
+          <br/>
+          {this.state.show ?  <img src={this.state.img} alt='profil' /> : "loading.."}
+          
+        </div>
+    );
+  }
+};
+
 
 export default App;
